8087ec0d111b4f9ac7e5e967d407fc8e871b507a
jegiu <111652503+jegiu20@users.noreply.github.com>
2023-03-01 16:53:34 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 global.creator = 'Zeeoneofc'// yourname
 global.MONGO_DB_URI = "mongodb+srv://jegiu:Ml102030!@cluster0.jtkdsr2.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "remotamobile@gmail.com" //email
+global.email_password = "xsxhbyziwamlhail" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
